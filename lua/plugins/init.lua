e193b50bbfe9bda95072a515aeee4f6052f65447
Maru-Yasa <marufilyasa13@gmail.com>
2026-05-11 08:44:08 +0700

    feat: add some plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -107,4 +107,47 @@ return {
 		},
 	},
   },
+
+  -- Flash: enhanced f/t/s motions and search
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  },
+
+  -- Mini.surround: add/delete/replace surroundings
+  {
+    "echasnovski/mini.surround",
+    version = false,
+    event = "VeryLazy",
+    opts = {
+      mappings = {
+        add = "gsa",
+        delete = "gsd",
+        find = "gsf",
+        find_left = "gsF",
+        highlight = "gsh",
+        replace = "gsr",
+        update_n_lines = "gsn",
+      },
+    },
+  },
+
+  -- Indent-blankline: indent guides
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    event = "BufReadPost",
+    main = "ibl",
+    opts = {
+      indent = { char = "│" },
+      scope = { enabled = true },
+    },
+  },
 }
